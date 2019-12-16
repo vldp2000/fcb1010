5fa1d71756e6493da27c8fecbba4fc7d3cf02446
vldp2000 <42130064+vldp2000@users.noreply.github.com>
2019-12-16 21:25:29 +1100

    build generic update and insert

--- dbapi/dbaccess.js
+++ dbapi/dbaccess.js
@@ -30,23 +30,64 @@ exports.getTableRecord = function(tableName, id, callback) {
 }
 
 
-exports.buildQuery = function(tableName, jsonData) {
+exports.buildUpdateQuery = function(tableName, jsonData) {
     let sql = 'UPDATE '+ tableName + ' set ';
     let id = '';
     for (let prop in jsonData ) {
-        if (prop.startsWith('v_')) {
-            console.log('skip');
+        if (prop.startsWith('v_') || prop.toUpperCase() == 'ID') {
+            //console.log(prop.toUpperCase());
+            //console.log('skip');
         }
         else {
+            //console.log(prop.toUpperCase());
             sql += prop + " = '" + jsonData[prop]+ "' ,";
-            if ( prop === 'id')
-                id = jsonData[prop];
+        }
+        if ( prop === 'id') {
+            id = jsonData[prop];
         }
     }
     if (sql.endsWith(','))
         sql = sql.slice(0, sql.length -1 );
     sql += ' where id = '+ id;    
+    sql = sql.split( "'null'" ).join( 'null' );
     console.log(sql);
-    console.log(id);
+    return sql;
 }
 
+exports.buildInsertQuery = function(tableName, jsonData) {
+    let sql = 'Insert Into '+ tableName ;
+    let fields = ' (';
+    let values = ' (';
+    for (let prop in jsonData ) {
+        if (prop.startsWith('v_') || prop.toUpperCase() == 'ID') {
+            //console.log(prop.toUpperCase());
+            //console.log('skip');
+        }
+        else {
+            //console.log(prop.toUpperCase());
+            fields += prop + "," 
+            values += "'" + jsonData[prop] + "' ,";
+        }
+
+        if ( prop === 'id') {
+            id = jsonData[prop];
+        }
+    }
+    if (fields.endsWith(','))
+        fields = fields.slice(0, fields.length -1 );
+    if (values.endsWith(','))
+        values = values.slice(0, values.length -1 );    
+    sql += fields + ' )  values (' + values + ') ' ;   
+    sql = sql.split( "'null'" ).join( 'null' );
+
+    //console.log(sql);
+    return sql;
+
+    /*
+    last_insert_rowid()
+    The last_insert_rowid() function returns the ROWID of the last 
+    row insert from the database connection which invoked the function. 
+    The last_insert_rowid() SQL function is a wrapper around the 
+    sqlite3_last_insert_rowid() C/C++ interface function.
+    */
+}
--- dbapi/mididbapi.js
+++ dbapi/mididbapi.js
@@ -64,7 +64,8 @@ function parceJsonObject(jsonData) {
 
 app.get("/api/update/:table/", (req, res, next) => {
     const songdata = { id: 1, name: 'The Star', tempo: null , v_dummy1:'qqqqq', v_dummy2:'zzzzzz' }
-    dbaccess.buildQuery(req.params.table, songdata);
+    let sql = dbaccess.buildUpdateQuery(req.params.table, songdata);
+    console.log(sql);
     res.json({"message":"Ok"});
     //{
         //buildQuery(data);
@@ -72,6 +73,16 @@ app.get("/api/update/:table/", (req, res, next) => {
     //}
 });
 
+app.get("/api/insert/:table/", (req, res, next) => {
+    const songdata = { id: 1, name: 'The Star', tempo: null , v_dummy1:'qqqqq', v_dummy2:'zzzzzz' }
+    let sql = dbaccess.buildInsertQuery(req.params.table, songdata);
+    console.log(sql);
+    res.json({"message":"Ok"});
+    //{
+        //buildQuery(data);
+        //res.json(data);
+    //}
+});
 app.get("/api/databyid/:table/:id", (req, res, next) => {
     dbaccess.getTableRecord(req.params.table, req.params.id, function(data){
         res.json(data);
