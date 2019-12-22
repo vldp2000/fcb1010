493c35128a9410d48ba72b38a7a5d7aebfd6679d
vldp2000 <42130064+vldp2000@users.noreply.github.com>
2019-12-22 23:26:40 +1100

    vue only version

---
+++ maintananceapp/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-app>
+    <v-app-bar
+      app
+      color="primary"
+      dark
+    >
+
+    </v-app-bar>
+
+    <v-content>
+      <SongList/>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+import SongList from './components/SongList';
+
+export default {
+  name: 'App',
+
+  components: {
+    SongList,
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
+
---
+++ maintananceapp/src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <SongList msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import SongList from '@/components/SongList.vue'
+
+export default {
+  name: 'home',
+  components: {
+    SongList
+  }
+}
+</script>
