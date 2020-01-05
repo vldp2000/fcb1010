347eef9246b8a0d004a68a2e614d1aa5e8c61b48
vldp2000 <42130064+vldp2000@users.noreply.github.com>
2020-01-05 17:08:25 +1100

    add the expanded preset panel

---
+++ maintenance/src/components/SongPresetsPanel.vue
@@ -0,0 +1,63 @@
+<template>
+  <v-data-table
+    :headers="headers"
+    :items="presetList"
+    item-key="id"
+    class="elevation-1"
+  >
+  </v-data-table>
+</template>
+
+<script>
+// import { mapState } from 'vuex'
+
+export default {
+  name: 'SongPresetsPanel',
+  props: {
+    presetList: {
+      type: Array
+    }
+  },
+
+  data () {
+    return {
+      headers: [
+        {
+          text: 'name',
+          align: 'left',
+          sortable: false,
+          value: 'name'
+        },
+        { text: 'instrument', value: 'refinstrument' },
+        { text: 'instrumentbank', value: 'refinstrumentbank' },
+        { text: 'volume', value: 'volume' },
+        { text: 'pan', value: 'pan' },
+        { text: 'muteflag', value: 'muteflag' },
+        { text: 'reverbflag', value: 'reverbflag' },
+        { text: 'reverbvalue', value: 'reverbvalue' },
+        { text: 'delayflag', value: 'delayflag' },
+        { text: 'delayvalue', value: 'delayvalue' },
+        { text: 'modeflag', value: 'modeflag' }
+      ],
+      expanded: [],
+      singleExpand: false
+    }
+  },
+
+  // computed: {
+  //   ...mapState(['songList'])
+  // }
+
+  mounted () {
+  //   this.programList = this.selectedSong.programList
+    console.log('----->SongPresetsPanel')
+    console.log(this.presetList)
+  }
+
+  // watch: {
+  //   songList: function (newValue, oldValue) {
+  //     this.programList = newValue.programList
+  //   }
+  // }
+}
+</script>
--- maintenance/src/components/SongProgramsPanel.vue
+++ maintenance/src/components/SongProgramsPanel.vue
@@ -4,15 +4,32 @@
     :items="programList"
     item-key="id"
     class="elevation-1"
+    @click:row="rowClicked"
+    :single-expand="singleExpand"
+    :expanded.sync="expanded"
   >
+    <template v-slot:expanded-item="{ headers }">
+      <td :colspan="headers.length">
+        <div>
+          <song-presets-panel v-bind:presetList="selectedPresetList" >
+
+          </song-presets-panel>
+        </div>
+      </td>
+    </template>
+
   </v-data-table>
 </template>
 
 <script>
 // import { mapState } from 'vuex'
+import SongPresetsPanel from '@/components/SongPresetsPanel'
 
 export default {
   name: 'SongProgramsPanel',
+  components: {
+    SongPresetsPanel
+  },
   props: {
     programList: {
       type: Array
@@ -29,7 +46,10 @@ export default {
           value: 'name'
         },
         { text: 'midipedal', value: 'midipedal' }
-      ]
+      ],
+      selectedPresetList: [],
+      expanded: [],
+      singleExpand: false
     }
   },
 
@@ -41,6 +61,20 @@ export default {
   //   this.programList = this.selectedSong.programList
     console.log('----->ongProgramsPanel')
     console.log(this.programList)
+  },
+  methods: {
+    async rowClicked (value) {
+      console.log(`<SongProgramPanel row clicked> ${value.id}`)
+      console.log(value)
+
+      if (this.expanded.length > 0) {
+        this.expanded.pop()
+      } else {
+        this.selectedPresetList = value.presetList
+        this.expanded.push(value)
+        console.log(this.selectedPresetList)
+      }
+    }
   }
 
   // watch: {
