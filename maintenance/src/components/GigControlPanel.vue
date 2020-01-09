4be6f14857886a60c7eabe41d7cd0000641abd94
vldp2000 <42130064+vldp2000@users.noreply.github.com>
2020-01-10 00:21:44 +1100

    set the colours

--- maintenance/src/components/GigControlPanel.vue
+++ maintenance/src/components/GigControlPanel.vue
@@ -1,11 +1,15 @@
 <template>
  <v-container grid-list-md text-md-center fluid fill-height class="darkBackgroud">
 
-      <v-row no-gutters>
+      <v-row no-gutters >
 
-        <div class="rotated-up">
-          <b>Program A</b>
-        </div>
+          <!-- <div class='rotatedLabel'>
+            <b>Program A</b>
+          </div> -->
+
+          <div class="progLabel">
+            <h1>A</h1>
+          </div>
 
         <v-col md3 d-flex>
           <v-card class="pa-1" dark >
@@ -30,40 +34,39 @@
 
       </v-row>
 
-      <v-row no-gutters>
-        <b>Program B</b>
+      <v-row md12 no-gutters>
+
+          <div class="progLabelSelected">
+            <h1>B</h1>
+          </div>
+
+        <v-col md3 d-flex>
+          <v-card  class="pa-1" dark >
+            <preset  />
+          </v-card>
+        </v-col>
+        <v-col md3 d-flex>
+          <v-card  class="pa-1" dark >
+            <preset  />
+          </v-card>
+        </v-col>
+        <v-col md3 d-flex>
+          <v-card  class="pa-1" dark >
+            <preset  />
+          </v-card>
+        </v-col>
+        <v-col md3 d-flex>
+          <v-card  class="pa-1" dark >
+            <preset  />
+          </v-card>
+        </v-col>
+
       </v-row>
 
       <v-row md12 no-gutters>
-
-        <v-col md3 d-flex>
-          <v-card  class="pa-1" dark >
-            <preset  />
-          </v-card>
-        </v-col>
-        <v-col md3 d-flex>
-          <v-card  class="pa-1" dark >
-            <preset  />
-          </v-card>
-        </v-col>
-        <v-col md3 d-flex>
-          <v-card  class="pa-1" dark >
-            <preset  />
-          </v-card>
-        </v-col>
-        <v-col md3 d-flex>
-          <v-card  class="pa-1" dark >
-            <preset  />
-          </v-card>
-        </v-col>
-
-      </v-row>
-
-      <v-row md12 no-gutters>
-        <b>Program C</b>
-      </v-row>
-
-      <v-row md12 no-gutters>
+          <div class="progLabel">
+            <h1>C</h1>
+          </div>
 
         <v-col md3 d-flex>
           <v-card  class="pa-1" dark >
@@ -88,10 +91,9 @@
       </v-row>
 
       <v-row md12 no-gutters>
-        <b>Program D</b>
-      </v-row>
-
-      <v-row md12 no-gutters>
+          <div class="progLabel">
+            <h1>D</h1>
+          </div>
         <v-col md3 d-flex>
           <v-card  class="pa-1" dark >
             <preset  />
@@ -129,18 +131,36 @@
     background-color:rgba(36, 34, 34, 0.856)
   }
 
-.rotated-up {
-  width: 20px;
-  height: 80px;
-  background-color: blueviolet;
-  -ms-transform: rotate(-90deg);
-  -moz-transform: rotate(-90deg);
-  -webkit-transform: rotate(-90deg);
-  transform: rotate(-90deg);
-  filter: none; /* Mandatory for IE9 to show the vertical text correctly */
-  -ms-transform-origin: center center 0;
-  -moz-transform-origin: center center 0;
-  -webkit-transform-origin: center center  0;
-  transform-origin: center center 0;
+.progLabel {
+  text-align: center;
+  text-justify: auto;
+  color:black;
+  border: 2px solid black;
+  border-radius: 10px;
+
+  width: 40px;
+  height: 50px;
+  /* margin: 50px, 10px, 0px, -10px; */
+  margin-top: 50px;
+  margin-left: -10px;
+  margin-right: 2px;
+  padding: 0px;
 }
+
+.progLabelSelected {
+  text-align: center;
+  text-justify: auto;
+  color: blue;
+  border: 2px solid blue;
+  border-radius: 10px;
+
+  width: 40px;
+  height: 50px;
+  /* margin: 50px, 10px, 0px, -10px; */
+  margin-top: 50px;
+  margin-left: -10px;
+  margin-right: 2px;
+  padding: 0px;
+}
+
 </style>
