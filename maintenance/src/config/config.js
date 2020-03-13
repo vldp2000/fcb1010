32f0a7522815f86430e1a5a9ab4594072da1983f
vldp2000 <42130064+vldp2000@users.noreply.github.com>
2020-03-13 23:07:19 +1100

    set hostnape for rpi

--- maintenance/src/config/config.js
+++ maintenance/src/config/config.js
@@ -7,6 +7,6 @@ module.exports = {
   controllerSyncMessage: process.env.VUE_APP_CONTROLLER_SYNC_MESSAGE || 'CONTROLLER_SYNC_MESSAGE',
   controllerPedal1Message: process.env.VUE_APP_CONTROLLER_PEDAL1_MESSAGE || 'CONTROLLER_PEDAL1_MESSAGE',
   controllerPedal2Message: process.env.VUE_APP_CONTROLLER_PEDAL2_MESSAGE || 'CONTROLLER_PEDAL2_MESSAGE',
-  API_URL: process.env.VUE_APP_API_URL || 'http://192.168.17.55:8081/',
-  messageURL: process.env.VUE_APP_MESSAGE_URL || 'http://192.168.17.55:8081/'
+  API_URL: process.env.VUE_APP_API_URL || 'http://vpmidi:8081/',
+  messageURL: process.env.VUE_APP_MESSAGE_URL || 'http://vpmidi:8081/'
 }
