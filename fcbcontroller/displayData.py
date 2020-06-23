4276632eecc90e91b8f23194f2906d5a2886b74b
Vlad Perepelytsya <vldp2000@gmail.com>
2020-06-23 23:21:24 +1000

    Update displayData.py

--- fcbcontroller/displayData.py
+++ fcbcontroller/displayData.py
@@ -16,60 +16,61 @@ DC = 23
 SPI_PORT = 0
 SPI_DEVICE = 0
 
+g_Disp = None
+
 # 128x64 display with hardware I2C:
 # disp = Adafruit_SSD1306.SSD1306_128_64(rst=RST)
 
 # Note you can change the I2C address by passing an i2c_address parameter like:
 
 def initDisplay():
-disp = Adafruit_SSD1306.SSD1306_128_64(rst=RST, i2c_address=0x3C)
-# Initialize library.
-disp.begin()
+  global g_Disp
+  g_Disp = Adafruit_SSD1306.SSD1306_128_64(rst=RST, i2c_address=0x3C)
+  # Initialize library.
+  g_Disp.begin()
 
-# Clear display.
-disp.clear()
-disp.display()
+  # Clear display.
+  g_Disp.clear()
+  g_Disp.display()
 
-# Create blank image for drawing.
-# Make sure to create image with mode '1' for 1-bit color.
-width = disp.width
-height = disp.height
-image = Image.new('1', (width, height))
 
-# Get drawing object to draw on image.
-draw = ImageDraw.Draw(image)
-
-# Draw a black filled box to clear the image.
-draw.rectangle((0,0,width,height), outline=0, fill=0)
-
-# Draw some shapes.
 # First define some constants to allow easy resizing of shapes.
-padding = 0
-top = padding
-bottom = height-padding
-# Move left to right keeping track of the current x position for drawing shapes.
-x = 0
+#  top = 0
+#  bottom = height-top
+
+def clearScreen():
+  global g_Disp
+  # Create blank image for drawing.
+  # Make sure to create image with mode '1' for 1-bit color.
+  width = disp.width
+  height = disp.height
+  image = Image.new('1', (width, height))
+
+  # Get drawing object to draw on image.
+  draw = ImageDraw.Draw(image)
+
+  # Draw a black filled box to clear the image.
+  draw.rectangle((0,0,width,height), outline=0, fill=0)
+
+def drawScreen():
+  font1 = ImageFont.truetype('8-bit-pusab.ttf', 11)
+  font2 = ImageFont.truetype('8-bit-pusab.ttf', 11)
+  font3 = ImageFont.truetype('8-bit-pusab.ttf', 11)
+  # Some other nice fonts to try: http://www.dafont.com/bitmap.php
 
 
-# Load default font.
-#font = ImageFont.load_default()
+  draw.text((x, top),       "Test 1 ", font=font1, fill=255)
+  draw.text((x, top+18),    "TEST 2" , font=font2, fill=255)
+  draw.text((x, top+40),    "TeSt 4" , font=font3, fill=255)
 
-# Alternatively load a TTF font.  Make sure the .ttf font file is in the same directory as the python script!
-# Some other nice fonts to try: http://www.dafont.com/bitmap.php
-font1 = ImageFont.truetype('8-bit-pusab.ttf', 11)
-font2 = ImageFont.truetype('8-bit-pusab.ttf', 11)
-font3 = ImageFont.truetype('8-bit-pusab.ttf', 11)
+  # Display image.
+  disp.image(image)
+  disp.display()
+  time.sleep(.1)
 
-# Draw a black filled box to clear the image.
-draw.rectangle((0,0,width,height), outline=0, fill=0)
 
-# Write two lines of text.
-
-draw.text((x, top),       "Test 1 ", font=font1, fill=255)
-draw.text((x, top+18),    "TEST 2" , font=font2, fill=255)
-draw.text((x, top+40),    "TeSt 4" , font=font3, fill=255)
-
-# Display image.
-disp.image(image)
-disp.display()
-time.sleep(.1)
+initDisplay()
+clearScreen()
+drawScreen()
+time.sleep(10)
+clearScreen()
