b6b054a5e817f3a3d13cc97a2b27b926a35a6cb0
Vlad Perepelytsya <vldp2000@gmail.com>
2020-06-23 23:46:51 +1000

    Update displayData.py

--- fcbcontroller/displayData.py
+++ fcbcontroller/displayData.py
@@ -56,9 +56,9 @@ def drawStatus():
  
 def drawScreen():
   global g_Disp
-  font1 = ImageFont.truetype('8-bit-pusab.ttf', 11)
-  font2 = ImageFont.truetype('UAV-OSD-Mono.ttf', 11)
-  font3 = ImageFont.truetype('UAV-OSD-Sans-Mono.ttf', 11)
+  font1 = ImageFont.truetype('8-bit-pusab.ttf', 10)
+  font2 = ImageFont.truetype('8-Bit Madness.ttf', 11)
+  font3 = ImageFont.truetype('UAV-OSD-Sans-Mono.ttf', 10)
   # Some other nice fonts to try: http://www.dafont.com/bitmap.php
   image = Image.new('1', (g_Disp.width, g_Disp.height))
   # Get drawing object to draw on image.
