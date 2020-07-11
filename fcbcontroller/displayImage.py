7205e3c40acf0c50b8a741fb2333f277388d1456
Vlad Perepelytsya <vldp2000@gmail.com>
2020-07-11 14:37:59 +1000

    add image

--- fcbcontroller/displayImage.py
+++ fcbcontroller/displayImage.py
@@ -31,11 +31,15 @@ disp.clear()
 disp.display()
 
 # Load image based on OLED display height.  Note that image is converted to 1 bit color.
-if disp.height == 64:
-    image = Image.open('VGMates.png').convert('1')
-# Alternatively load a different format image, resize it, and convert to 1 bit color.
-#image = Image.open('happycat.png').resize((disp.width, disp.height), Image.ANTIALIAS).conv$
+image = Image.open('VGMates.png').convert('1')
 
+# Display image.
+disp.image(image)
+disp.display()
+
+time.sleep(10)
+
+image = Image.open('VGMatesBlack.png').convert('1')
 # Display image.
 disp.image(image)
 disp.display()
