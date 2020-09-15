35d8e34b15e7a00a3b48e71a2cf4d16390314c0f
Vlad Perepelytsya <vldp2000@gmail.com>
2020-09-15 22:09:13 +1000

    Update dataHelper.py

--- fcbcontroller/dataHelper.py
+++ fcbcontroller/dataHelper.py
@@ -59,9 +59,10 @@ def reloadSong(allSongs, id):
   data = dataController.getSong(id)
   for i in range(4):
     print ('-----------------------')
-    print data['programList'][i]
+    print (data['programList'][i])
+
     program = song.programList[i]
-    programNew = data.['programList'][i]
+    programNew = data['programList'][i]
     for x in range(4):
       program['presetList'][x]['volume'] = programNew['presetList'][x]['volume']
       program['presetList'][x]['delayflag'] = programNew['presetList'][x]['delayflag']
