a3d9ee368fb103d8a900e3162f2965ff7ebd6c09
Vlad Perepelytsya <vldp2000@gmail.com>
2020-09-15 22:18:44 +1000

    Update fcbcontroller.py

--- fcbcontroller/fcbcontroller.py
+++ fcbcontroller/fcbcontroller.py
@@ -692,9 +692,8 @@ def setPreset(program, songPreset):
   if preset['refinstrument'] == 1:
     displayData.setProgramName(f"{program['name']}.{preset['name']}")
     displayData.drawScreen()
-  
+    
   printDebug("preset volume" + str(songPreset['volume']))
-
 #----------------------------------------------------------------
 
 def selectNextSong():
