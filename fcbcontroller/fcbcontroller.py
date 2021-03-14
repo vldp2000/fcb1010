2fb5ef17178d5e3a154541505d9ba611f8f73705
vldp2000 <42130064+vldp2000@users.noreply.github.com>
2021-03-14 19:19:12 +1100

    Update fcbcontroller.py

--- fcbcontroller/fcbcontroller.py
+++ fcbcontroller/fcbcontroller.py
@@ -165,7 +165,7 @@ def processProgramMessage(idx):
 def processControllerModeMessage(payload):
   global gMode
   printDebug(f"->->  Received message VIEW_EDIT_MODE_MESSAGE = {payload}")
-  if str(payload) == '0':
+  if str(payload) == '1':
     gMode = 'Config'
     displayData.drawMessage("Mode","Config")
   else:    
