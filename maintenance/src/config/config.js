81b446a6fc5ed7e409f47f214a6107569776bbab
vldp2000 <42130064+vldp2000@users.noreply.github.com>
2021-03-14 14:44:04 +1100

    Update config.js

--- maintenance/src/config/config.js
+++ maintenance/src/config/config.js
@@ -1,14 +1,12 @@
-module.exports = {
-  viewProgramMessage: process.env.VUE_APP_VIEW_PROGRAM_MESSAGE || 'VIEW_PROGRAM_MESSAGE',
-  viewSongMessage: process.env.VUE_APP_VIEW_SONG_MESSAGE || 'VIEW_SONG_MESSAGE',
-  viewEditModeMessage: process.env.VUE_APP_VIEW_EDIT_MODE_MESSAGE || 'VIEW_EDIT_MODE_MESSAGE',
-  controllerPresetVoluleMessage: process.env.VUE_APP_CONTROLLER_PRESET_VOLUME_MESSAGE || 'CONTROLLER_PRESET_VOLUME_MESSAGE',
-  controllerProgramMessage: process.env.VUE_APP_CONTROLLER_PROGRAM_MESSAGE || 'CONTROLLER_PROGRAM_MESSAGE',
-  controllerSongMessage: process.env.VUE_APP_CONTROLLER_SONG_MESSAGE || 'CONTROLLER_SONG_MESSAGE',
-  controllerGigMessage: process.env.VUE_APP_CONTROLLER_GIG_MESSAGE || 'CONTROLLER_GIG_MESSAGE',
-  controllerSyncMessage: process.env.VUE_APP_CONTROLLER_SYNC_MESSAGE || 'CONTROLLER_SYNC_MESSAGE',
-  controllerPedal1Message: process.env.VUE_APP_CONTROLLER_PEDAL1_MESSAGE || 'CONTROLLER_PEDAL1_MESSAGE',
-  controllerPedal2Message: process.env.VUE_APP_CONTROLLER_PEDAL2_MESSAGE || 'CONTROLLER_PEDAL2_MESSAGE',
-  API_URL: 'http://192.168.37.2:8081/',
-  messageURL: 'http://192.168.37.2:8081/'
-}
+export const viewProgramMessage = process.env.VUE_APP_VIEW_PROGRAM_MESSAGE || 'VIEW_PROGRAM_MESSAGE';
+export const viewSongMessage = process.env.VUE_APP_VIEW_SONG_MESSAGE || 'VIEW_SONG_MESSAGE';
+export const viewEditModeMessage = process.env.VUE_APP_VIEW_EDIT_MODE_MESSAGE || 'VIEW_EDIT_MODE_MESSAGE';
+export const controllerPresetVoluleMessage = process.env.VUE_APP_CONTROLLER_PRESET_VOLUME_MESSAGE || 'CONTROLLER_PRESET_VOLUME_MESSAGE';
+export const controllerProgramMessage = process.env.VUE_APP_CONTROLLER_PROGRAM_MESSAGE || 'CONTROLLER_PROGRAM_MESSAGE';
+export const controllerSongMessage = process.env.VUE_APP_CONTROLLER_SONG_MESSAGE || 'CONTROLLER_SONG_MESSAGE';
+export const controllerGigMessage = process.env.VUE_APP_CONTROLLER_GIG_MESSAGE || 'CONTROLLER_GIG_MESSAGE';
+export const controllerSyncMessage = process.env.VUE_APP_CONTROLLER_SYNC_MESSAGE || 'CONTROLLER_SYNC_MESSAGE';
+export const controllerPedal1Message = process.env.VUE_APP_CONTROLLER_PEDAL1_MESSAGE || 'CONTROLLER_PEDAL1_MESSAGE';
+export const controllerPedal2Message = process.env.VUE_APP_CONTROLLER_PEDAL2_MESSAGE || 'CONTROLLER_PEDAL2_MESSAGE';
+export const API_URL = 'http://192.168.37.2:8081/';
+export const messageURL = 'http://192.168.37.2:8081/';
