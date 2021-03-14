a46b2a9a9a88098f40ef6649e73176d3fd7f0ee2
vldp2000 <42130064+vldp2000@users.noreply.github.com>
2021-03-14 14:27:51 +1100

    remove spaces

--- apiserver/src/config/config.js
+++ apiserver/src/config/config.js
@@ -4,11 +4,9 @@ module.exports = {
   httpPort: process.env.PORT || 8081,
   messagePort: 8081,
   filePath: './data/',
-  
   viewProgramMessage: 'VIEW_PROGRAM_MESSAGE',
   viewSongMessage: 'VIEW_SONG_MESSAGE',
   viewEditModeMessage: 'VIEW_EDIT_MODE_MESSAGE',
-
   controllerProgramMessage: 'CONTROLLER_PROGRAM_MESSAGE',
   controllerSongMessage: 'CONTROLLER_SONG_MESSAGE',
   controllerSyncMessage: 'CONTROLLER_SYNC_MESSAGE',
@@ -16,5 +14,4 @@ module.exports = {
   controllerPedal1Message: 'CONTROLLER_PEDAL1_MESSAGE',
   controllerPedal2Message: 'CONTROLLER_PEDAL2_MESSAGE',
   controllerPresetVoluleMessage: 'CONTROLLER_PRESETVOLUME_MESSAGE'
-  
 }
--- maintenance/src/config/config.js
+++ maintenance/src/config/config.js
@@ -2,13 +2,11 @@ module.exports = {
   viewProgramMessage: process.env.VUE_APP_VIEW_PROGRAM_MESSAGE || 'VIEW_PROGRAM_MESSAGE',
   viewSongMessage: process.env.VUE_APP_VIEW_SONG_MESSAGE || 'VIEW_SONG_MESSAGE',
   viewEditModeMessage: process.env.VUE_APP_VIEW_EDIT_MODE_MESSAGE || 'VIEW_EDIT_MODE_MESSAGE',
-
   controllerPresetVoluleMessage: process.env.VUE_APP_CONTROLLER_PRESET_VOLUME_MESSAGE || 'CONTROLLER_PRESET_VOLUME_MESSAGE',
   controllerProgramMessage: process.env.VUE_APP_CONTROLLER_PROGRAM_MESSAGE || 'CONTROLLER_PROGRAM_MESSAGE',
   controllerSongMessage: process.env.VUE_APP_CONTROLLER_SONG_MESSAGE || 'CONTROLLER_SONG_MESSAGE',
   controllerGigMessage: process.env.VUE_APP_CONTROLLER_GIG_MESSAGE || 'CONTROLLER_GIG_MESSAGE',
   controllerSyncMessage: process.env.VUE_APP_CONTROLLER_SYNC_MESSAGE || 'CONTROLLER_SYNC_MESSAGE',
-  
   controllerPedal1Message: process.env.VUE_APP_CONTROLLER_PEDAL1_MESSAGE || 'CONTROLLER_PEDAL1_MESSAGE',
   controllerPedal2Message: process.env.VUE_APP_CONTROLLER_PEDAL2_MESSAGE || 'CONTROLLER_PEDAL2_MESSAGE',
   API_URL: 'http://192.168.37.2:8081/',
